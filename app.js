e379a72eece9372ee49ebc572a71693ed7b802ec
Aakashijaz786 <aakashijaz2002@example.com>
2025-05-07 12:30:08 +0500

    node app commited successfully

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("This is a simple Node.js application.");
+
+console.log("It demonstrates how to use console.log to print messages to the console.");
+console.log("You can run this application using Node.js from the command line.");
